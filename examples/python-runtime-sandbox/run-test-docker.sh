59d2572c9c4c8043f74cd68a6425b5b6f9413742
tomergee <83163454+tomergee@users.noreply.github.com>
2025-10-01 21:28:03 +0000

    Additional example of runnint a Python code execution runtime in a  kind cluster using agent-sandbox controller some cleanup
    Added files: examples/python-runtime-sandbox/run-test-kind.sh, examples/python-runtime-sandbox/sandbox-python-kind.yaml
    Renamed files: examples/python-runtime-sandbox/run-test -> examples/python-runtime-sandbox/run-test-docker.sh
    Modified files: dev/tools/push-images, examples/python-runtime-sandbox/README

--- examples/python-runtime-sandbox/run-test-docker.sh
+++ examples/python-runtime-sandbox/run-test-docker.sh
@@ -7,7 +7,7 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law-or-agreed to in writing, software
+# Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
@@ -40,4 +40,3 @@ python3 tester.py 127.0.0.1 8888
 
 echo "Test finished."
 # The 'trap' command will now execute the cleanup function to stop the container
-
