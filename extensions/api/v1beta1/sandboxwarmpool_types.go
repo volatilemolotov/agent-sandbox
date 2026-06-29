ed4a6d5e1e4252408562cd0d13ff295ebafd6950
Prashanth Venkatesh <prash.v.2512@gmail.com>
2026-06-28 17:59:04 -0700

    feat: add Desired printer column to SandboxWarmPool kubectl output (#1053)
    Add a "Desired" printer column to SandboxWarmPool (both v1beta1 and
    v1alpha1) so that `kubectl get sandboxwarmpool` shows the desired
    replica count alongside the ready count.
    
    Before:
      NAME                  READY   AGE
      my-sandbox-warmpool   2       5m
    
    After:
      NAME                  READY   DESIRED   AGE
      my-sandbox-warmpool   2       4         5m
    
    Ref #188

--- extensions/api/v1beta1/sandboxwarmpool_types.go
+++ extensions/api/v1beta1/sandboxwarmpool_types.go
@@ -98,6 +98,7 @@ type SandboxWarmPoolStatus struct {
 // +kubebuilder:subresource:scale:specpath=.spec.replicas,statuspath=.status.replicas,selectorpath=.status.selector
 // +kubebuilder:resource:scope=Namespaced,shortName=swp
 // +kubebuilder:printcolumn:name="Ready",type="integer",JSONPath=".status.readyReplicas"
+// +kubebuilder:printcolumn:name="Desired",type="integer",JSONPath=".spec.replicas"
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:storageversion
 // +kubebuilder:conversion:strategy=Webhook
